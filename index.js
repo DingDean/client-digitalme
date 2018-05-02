0e47fd47a3f4b8414ca112d7773696f0ecce7f09
DingDean <dingkework@hotmail.com>
2018-05-02 16:35:34 +0800

    index: use commander for cli

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const debug = require('debug')('digitme')
+const editorListener = require('./src/editorListen.js');
+const relay = require('./src/relay.js')
+const handle = require('./src/handler.js')
+
+program
+  .description( 'cli tool for digitalme' )
+  .option('--host <host>', 'remote server host')
+  .option('--port <port>', 'remote server port')
+  .option('--eport <eport>', 'local editor channel port')
+  .parse( process.argv )
+
+let { host, port, eport } = program
+
+host = host || "localhost"
+port = port || 8764
+eport = eport || 8763
+
+relay.remoteConnect( host, port );
+
+editorListener.listen( eport, msg => {
+  if (!msg)
+    return
+  let index, message
+  try {
+    let [i, m] = JSON.parse(msg)
+    index = i
+    message = m
+  } catch (e) {
+    debug('收到无效的编辑器消息')
+  }
+  let sender = relay.getGate()
+  if ( sender )
+    handle( sender, message )
+  // TODO: add a queue for messages
+})
