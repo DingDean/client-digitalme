6c738fbc88e3a303635f837c0f822dad8eb6059a
DingDean <dingkework@hotmail.com>
2018-05-03 13:36:19 +0800

    relay: monitor zeromq socket events
    After a change of network, such as switch on/off VPN, switch to another
    network, it can be a while before the socket reconnect. The disconnect
    and reconnect should be monitored and may be optimized later in the
    project.

--- src/relay.js
+++ src/relay.js
@@ -2,13 +2,32 @@ const debug = require('debug')('digitme')
 const zmq = require('zeromq')
 
 module.exports = function ( path ) {
-  const requester = zmq.socket('req')
+  const requester = zmq.socket('push')
 
-  requester.on('message', reply => {
-    debug("Received reply: [", reply.toString(), "]")
+  requester.on('connect', () => {
+    debug('Connected to remote server: ' + path)
   })
 
-  debug('Connectiong to remote server: ' + path)
-  requester.connect( path )
+  requester.on('close', () => {
+    debug('Socket is closed')
+  })
+
+  requester.on('disconnect', () => {
+    debug('Socket is disconnected')
+  })
+
+  requester.on('accept_error', err => {
+    debug('accept error: ' + err)
+  })
+
+  requester.on('error', err => {
+    debug('Error with zeromq' + err)
+  } )
+
+  requester.on('monitor_error', err => {
+    debug("Monitor catch error" + err)
+  })
+
+  requester.connect( path ).monitor()
   return requester;
 }
