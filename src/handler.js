65cc78c2bc7cf994e25ed55e0edc73e1f32b3f46
DingDean <dingkework@hotmail.com>
2018-05-05 16:50:16 +0800

    doc: new roadmap

--- src/handler.js
+++
@@ -1,9 +0,0 @@
-const debug = require('debug')('digitme')
-module.exports = function ( sender, message ) {
-  let { event, data } = message
-  if ( event == "ping" ) {// pass through handler
-    message = JSON.stringify(message)
-    debug('sending ' + message)
-    sender.write( message )
-  }
-}
