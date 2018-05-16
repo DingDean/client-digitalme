534d168a8140d8b638998e0de90185a7f2ee8333
DingDean <dingkework@hotmail.com>
2018-05-16 11:37:08 +0800

    index: comform to eslint standard

--- index.js
+++ index.js
@@ -8,47 +8,45 @@ const net = require('net')
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
-const TEMP_DIR = path.resolve( os.homedir(), '.digitalme' )
+const TEMP_DIR = path.resolve(os.homedir(), '.digitalme')
 
 try {
-  fs.readdirSync( TEMP_DIR )
+  fs.readdirSync(TEMP_DIR)
   debug('TEMP DIR exists, safe to go')
 } catch (e) {
   try {
-    fs.mkdirSync( TEMP_DIR )
+    fs.mkdirSync(TEMP_DIR)
     debug('TEMP DIR is created')
   } catch (e) {
-    throw(e)
+    throw (e)
   }
 }
 
 program
-  .description( 'cli tool for digitalme' )
+  .description('cli tool for digitalme')
   .option('--host <host>', 'remote server host')
   .option('--port <port>', 'remote server port')
   .option('--eport <eport>', 'local editor channel port')
-  .parse( process.argv )
+  .parse(process.argv)
 
 let { host, port, eport } = program
 
-host = host || "localhost"
+host = host || 'localhost'
 port = port || 8764
 eport = eport || 8763
 
-let sender = relay( `tcp://${host}:${port}` )
+let sender = relay(`tcp://${host}:${port}`)
 sender.on('flush_history', () => {
-  fs.readdir( TEMP_DIR, (err, files) => {
-    if (err)
-      return debug('Failed to open temp dir')
+  fs.readdir(TEMP_DIR, (err, files) => {
+    if (err) { return debug('Failed to open temp dir') }
     for (let name of files) {
-      name = path.resolve( TEMP_DIR, name )
-      fs.readFile( name, 'utf8', (err, buff) =>{
-        if ( !err ) {
-          sender.send( buff )
+      name = path.resolve(TEMP_DIR, name)
+      fs.readFile(name, 'utf8', (err, buff) => {
+        if (!err) {
+          sender.send(buff)
           debug('A local history is sync to remote')
-          fs.unlink( name, err => {
-            if (err)
-              debug(`Error on deleting temp file ${name}`)
+          fs.unlink(name, err => {
+            if (err) { debug(`Error on deleting temp file ${name}`) }
           })
         }
       })
@@ -56,65 +54,62 @@ sender.on('flush_history', () => {
   })
 })
 
-const editorListener = net.createServer( c => {
-  c.setEncoding( 'utf8' )
+const editorListener = net.createServer(c => {
+  c.setEncoding('utf8')
   c.on('data', msg => {
-    if (!msg)
-      return
-    let index, message
+    if (!msg) { return }
+    let message
     try {
-      let [i, m] = JSON.parse(msg)
-      index = i
-      message = m
+      msg = JSON.parse(msg)
+      message = msg[1]
     } catch (e) {
       debug('收到无效的编辑器消息, error: ' + e)
       debug('消息为 ' + msg)
       return
     }
     let {event, ts, data} = message
-    editorListener.emit( event, ts, data )
+    editorListener.emit(event, ts, data)
   })
 })
 
 editorListener.on('error', err => {
-  throw(new Error(err))
+  throw (new Error(err))
 })
 
-editorListener.on('ping', ( ts, data ) => {
+editorListener.on('ping', (ts, data) => {
   let current = Session.current
   if (current) {
-    if (current.isExpired( ts )) {
+    if (current.isExpired(ts)) {
       current.close()
-      Session.stash( current )
+      Session.stash(current)
       let {filename, filetype} = current
-      let s = Session.new( filename, filetype )
+      let s = Session.new(filename, filetype)
       Session.current = s
     } else {
       current.beat()
     }
-  }
-  else {
+  } else {
     // this might happen when client is restarted
     let s = Session.new('', '')
     s.marked = true
     Session.current = s
     debug(`A marked session is created with index ${s.index}`)
   }
-  if ( sender.isAlive() ) {
+  if (sender.isAlive()) {
     let msg = JSON.stringify({event: 'digit_ping'})
-    sender.send( msg )
+    sender.send(msg)
   }
 })
 
-editorListener.on('bufEnter', ( ts, data ) => {
-  let {filename='test', filetype='test'} = data
+editorListener.on('bufEnter', (ts, data) => {
+  let {filename = 'test', filetype = 'test'} = data
   let current = Session.current
-  if ( current ) {
-    if ( current.isClosed() ) {
+  if (current) {
+    if (current.isClosed()) {
       // do nothing
-    } else if ( current.isExpired( ts ) ) {
+    } else if (current.isExpired(ts)) {
       current.close()
-      Session.stash( current )
+      Session.stash(current)
     } else {
       debug('Possible bug, create new session when old one is not closed')
     }
@@ -126,14 +121,13 @@ editorListener.on('bufEnter', ( ts, data ) => {
 editorListener.on('bufLeave', (ts, data) => {
   let current = Session.current
 
-  if ( !current )
-    return debug('Possible bug, no session when a bufleave')
-  current.close( data )
-  Session.stash( current )
+  if (!current) { return debug('Possible bug, no session when a bufleave') }
+  current.close(data)
+  Session.stash(current)
 })
 
-editorListener.listen( eport, () => {
-  debug("Listening vim input on " + port)
+editorListener.listen(eport, () => {
+  debug('Listening vim input on ' + port)
 })
 
 process.on('SIGINT', () => {
@@ -145,22 +139,20 @@ setInterval(() => {
   // TODO: 2018-05-06
   // gzip the history
   let history = Session.history
-    .filter( e => e.validate())
-  if ( history.length == 0 )
-    return
+    .filter(e => e.validate())
+  if (history.length === 0) { return }
   Session.history = []
 
   let msg = {event: 'digit_session', data: {ts: Date.now(), history}}
   msg = JSON.stringify(msg)
 
-  if ( sender.isAlive() ) {
-    sender.send( msg )
+  if (sender.isAlive()) {
+    sender.send(msg)
     debug('History is synced to remote server')
   } else {
-    let fp = path.resolve( TEMP_DIR, `./${Date.now()}_tmp` )
+    let fp = path.resolve(TEMP_DIR, `./${Date.now()}_tmp`)
     fs.writeFile(fp, msg, err => {
-      if ( err )
-        return debug('Failed to save history locally: ' + err)
+      if (err) { return debug('Failed to save history locally: ' + err) }
       debug('History is saved locally')
     })
   }
